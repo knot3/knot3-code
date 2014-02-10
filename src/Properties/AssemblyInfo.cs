51ecfb88523e7a6b015362d5101bda403be0296b
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-10 15:55:13 +0100

    Code Format

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.14.0")]
-[assembly: AssemblyFileVersion("2.0.14.0")]
+[assembly: AssemblyVersion("2.0.16.0")]
+[assembly: AssemblyFileVersion("2.0.16.0")]
--- tests/Tests/KnotData/Node_Tests.cs
+++ tests/Tests/KnotData/Node_Tests.cs
@@ -11,95 +11,73 @@ using Knot3.GameObjects;
 using Knot3.KnotData;
 using Knot3.RenderEffects;
 
-
-
 namespace Knot3.Node_Tests
 {
 
+	[TestFixture]
+	public class Node_Tests
+	{
 
-    [TestFixture]
-    public class Node_Tests
-    {
+		public static Node node1;
+		public static Node node2;
 
+		public Node_Tests()
+		{
+		}
 
-        public static Node node1;
-        public static Node node2;
+		[SetUp]
+		public void Initialize()
+		{
+			node1 = new Node(1, 1, 1);
+			node2 = new Node(1, 1, 1);
+		}
 
+		[Test]
+		public void Node_Vector_Tests()
+		{
+			// Assert.AreEqual(node1.Vector, ); // ???
+		}
 
-        public Node_Tests()
-        {
-        }
+		[Test]
+		public void Node_Operator_Tests()
+		{
+			Vector3 result;
 
+			result = node1 - node2;
+			Assert.AreEqual(result, Vector3.Zero);
 
+			result = node1 + node2;
+			//Assert.AreEqual(result, ); // ???
 
-        [SetUp]
-        public void Initialize()
-        {
-            node1 = new Node(1, 1, 1);
-            node2 = new Node(1, 1, 1);
-        }
+			Assert.IsTrue(node1 == node2);
+			Assert.IsFalse(node1 != node2);
+		}
 
+		[Test]
+		public void Node_Equals_Tests()
+		{
+			Assert.IsTrue(node1.Equals(node1));
+			Assert.IsTrue(node1.Equals(node2));
+			Assert.IsFalse(node1.Equals(null));
+			Assert.IsTrue(Node.Equals(node1, node2));
+		}
 
+		[Test]
+		public void Node_CenterBetween_Tests()
+		{
+		}
 
-        [Test]
-        public void Node_Vector_Tests()
-        {
-            // Assert.AreEqual(node1.Vector, ); // ???
-        }
+		[Test]
+		public void Node_Clone_Tests()
+		{
+			Object clonedNode = node1.Clone();
+			Assert.IsTrue(clonedNode.Equals(node1));
+		}
 
-
-
-        [Test]
-        public void Node_Operator_Tests()
-        {
-            Vector3 result;
-
-            result = node1 - node2;
-            Assert.AreEqual(result, Vector3.Zero);
-
-            result = node1 + node2;
-            //Assert.AreEqual(result, ); // ???
-
-           
-
-            Assert.IsTrue(node1 == node2);
-            Assert.IsFalse(node1 != node2);
-        }
-
-
-
-        [Test]
-        public void Node_Equals_Tests()
-        {
-            Assert.IsTrue(node1.Equals(node1));
-            Assert.IsTrue(node1.Equals(node2));
-            Assert.IsFalse(node1.Equals(null));
-            Assert.IsTrue(Node.Equals(node1, node2));
-        }
-
-
-
-        [Test]
-        public void Node_CenterBetween_Tests()
-        {
-
-        }
-
-
-
-        [Test]
-        public void Node_Clone_Tests()
-        {
-            Object clonedNode = node1.Clone();
-            Assert.IsTrue(clonedNode.Equals(node1));
-        }
-
-
-
-        [Test]
-        public void Node_ToString_Tests()
-        {            
-            Assert.AreEqual(node1.ToString(), "(1,1,1)");
-        }
-    }
+		[Test]
+		public void Node_ToString_Tests()
+		{
+			Assert.AreEqual(node1.ToString(), "(1,1,1)");
+		}
+	}
 }
