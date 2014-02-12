0fc91218f0e0974c1d27e5bf10a190a6ac7c0620
Gerd Augsburg <gerd.augsburg@student.kit.edu>
2014-02-12 21:09:12 +0100

    Code Format

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.71.0")]
-[assembly: AssemblyFileVersion("2.0.71.0")]
+[assembly: AssemblyVersion("2.0.74.0")]
+[assembly: AssemblyFileVersion("2.0.74.0")]
