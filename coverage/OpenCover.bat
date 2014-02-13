a0bcbb5771acabda2b5e7b5edc4ae4421919a13c
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-13 15:36:49 +0100

    Verbessertes Skript für die lokalen Test-Tools unter Windows.

--- coverage/OpenCover.bat
+++ coverage/OpenCover.bat
@@ -1,5 +1,6 @@
 echo.
 echo --- OpenCover.bat ---
+@echo off
 echo.
 ::
 :: Folgende Tools müssen installiert sein:
@@ -28,34 +29,39 @@ echo.
 ::
 :: ...
 ::
-set PATH_TO_OPENCOVER=%USERPROFILE%\AppData\Local\Apps\OpenCover
-set PATH_TO_NUNIT=%ProgramFiles(x86)%\NUnit 2.6.3\bin
-cd ..
-cd ..
-cd ..
-set PATH_TO_PROJECT=%CD%\tests\bin\Debug\Knot3.UnitTests.dll
-set PATH_TO_REPORTGENERATOR=%ProgramFiles(x86)%\ReportGenerator\bin
-echo.
 echo.
 echo Settings:
 echo.
 echo Current path: %CD%
 echo.
+
+
+::
+:: Standard-Installationsverzeichnisse für die Werkzeuge verwenden!
+::
+set PATH_TO_PROJECT=%~p0..
+set PATH_TO_OPENCOVER=%USERPROFILE%\AppData\Local\Apps\OpenCover
+set PATH_TO_NUNIT=%ProgramFiles(x86)%\NUnit 2.6.3\bin
+set PATH_TO_TESTS=%PATH_TO_PROJECT%\tests\bin\Debug
+set PATH_TO_REPORTGENERATOR=%ProgramFiles(x86)%\ReportGenerator\bin
+set PATH_TO_REPORT=%PATH_TO_PROJECT%\coverage\bin\Debug
+
 echo Paths to ...
 echo  NUnit           : %PATH_TO_NUNIT%
+echo  Tests           : %PATH_TO_TESTS%
 echo  OpenCover       : %PATH_TO_OPENCOVER%
 echo  VS-Project      : %PATH_TO_PROJECT%
 echo  ReportGenerator : %PATH_TO_REPORTGENERATOR%
+echo  Report          : %PATH_TO_REPORT%
 echo.
+
+
 ::
 :: Hinweis: Auf einem 64-Bit-System ist nunit-console-x86.exe für 32-Bit-Projekte zu verwenden! (sonst tritt ein Fehler auf)
 ::
-"%PATH_TO_OPENCOVER%\OpenCover.Console.exe" -target:"%PATH_TO_NUNIT%\nunit-console-x86.exe" -targetargs:"/noshadow "%USERPROFILE%\Documents\GitHub\knot3-code\tests\bin\Debug\Knot3.UnitTests.dll"" -register:user -filter:"+[*]*" -output:"%USERPROFILE%\Documents\GitHub\knot3-code\coverage\bin\Debug\NUnit_test_coverage.xml"
+"%PATH_TO_OPENCOVER%\OpenCover.Console.exe" -target:"%PATH_TO_NUNIT%\nunit-console-x86.exe" -targetargs:"/noshadow "%PATH_TO_TESTS%\Knot3.UnitTests.dll"" -register:user -filter:"+[*]*" -output:"%PATH_TO_REPORT%\NUnit_test_coverage.xml"
 echo.
-echo Current path: %CD%
-echo.
-set PATH_TO_REPORT=%CD%\NUnit-Test-Coverage-Windows\bin\Debug
-echo Path to report: %PATH_TO_REPORT%
-echo.
-"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" "%USERPROFILE%\Documents\GitHub\knot3-code\coverage\bin\Debug\NUnit_test_coverage.xml" "%USERPROFILE%\Documents\GitHub\knot3-code\coverage\bin\Debug\"
-start "" "%USERPROFILE%\Documents\GitHub\knot3-code\coverage\bin\Debug\index.htm"
+
+
+"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" "%PATH_TO_REPORT%\NUnit_test_coverage.xml" "%PATH_TO_REPORT%"
+start "" "%PATH_TO_REPORT%\index.htm"
