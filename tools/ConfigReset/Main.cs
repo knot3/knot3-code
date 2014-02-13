85ab253cb5a582a269ca515094a781ae9f54833e
Tobias Schulz <tobias@freehal.net>
2014-02-14 00:19:53 +0100

    Externes Tool zum Config resetten (pse-knot/pse-knot#145)

--- tools/ConfigReset/Main.cs
+++ tools/ConfigReset/Main.cs
@@ -1,12 +1,33 @@
 using System;
+using System.IO;
+
+using Knot3;
+using Knot3.Core;
+using Knot3.Utilities;
 
 namespace ConfigReset
 {
-	class MainClass
+	public class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Settings directory: " + FileUtility.SettingsDirectory);
+			Console.WriteLine ();
+			Console.WriteLine ("Press <Enter> to delete it.");
+			Console.ReadLine();
+
+			try {
+				Directory.Delete (FileUtility.SettingsDirectory, true);
+				Console.WriteLine ("Settings deleted.");
+			}
+			catch (Exception ex) {
+				Console.WriteLine ("Error: Could not delete settings!");
+				Console.WriteLine (ex.ToString ());
+			}
+			Console.WriteLine ();
+
+			Console.WriteLine("Press <Enter> to exit.");
+			Console.ReadLine();
 		}
 	}
 }
