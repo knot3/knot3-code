3c89f44b359f342e068f1c087882f9a5b9cd9de9
Daniel Warzel <daniel.warzel@gmx.de>
2014-02-13 17:01:12 +0100

    Code Format

--- tests/MockObjects/KnotGenerator.cs
+++ tests/MockObjects/KnotGenerator.cs
@@ -7,29 +7,28 @@ using System.Threading.Tasks;
 
 namespace Knot3.UnitTests.MockObjects
 {
-    class KnotGenerator
-    {
-        public static Knot generateValidSquaredKnot(int EdgeLength) 
-        {
-            Edge[] edgeList = new Edge[EdgeLength * 4];
+	class KnotGenerator
+	{
+		public static Knot generateValidSquaredKnot(int EdgeLength)
+		{
+			Edge[] edgeList = new Edge[EdgeLength * 4];
 
-            for(int i = 0; i < EdgeLength; i++) {
-                edgeList[i] = Edge.Up;
-            }
-            for(int i = EdgeLength; i < EdgeLength*2; i++) {
-                edgeList[i] = Edge.Right;
-            }
-            for(int i = EdgeLength *2; i < EdgeLength*3; i++) {
-                edgeList[i] = Edge.Down;
-            }
-            for(int i = EdgeLength *3; i < EdgeLength*4; i++) {
-                edgeList[i] = Edge.Left;
-            }
-            KnotMetaData metaData = new KnotMetaData("SquareKnot", edgeList.Count<Edge>, null, null);
-            Knot squareKnot = new Knot(metaData, edgeList);
-            
-            return squareKnot;
+			for(int i = 0; i < EdgeLength; i++) {
+				edgeList[i] = Edge.Up;
+			}
+			for(int i = EdgeLength; i < EdgeLength*2; i++) {
+				edgeList[i] = Edge.Right;
+			}
+			for(int i = EdgeLength *2; i < EdgeLength*3; i++) {
+				edgeList[i] = Edge.Down;
+			}
+			for(int i = EdgeLength *3; i < EdgeLength*4; i++) {
+				edgeList[i] = Edge.Left;
+			}
+			KnotMetaData metaData = new KnotMetaData("SquareKnot", edgeList.Count<Edge>, null, null);
+			Knot squareKnot = new Knot(metaData, edgeList);
 
-        }
-    }
+			return squareKnot;
+		}
+	}
 }
