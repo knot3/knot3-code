cd4e261d5020e4dd191113f0054c79181c13aea6
PSE Knot³ <uaeti@student.kit.edu>
2014-02-13 20:13:12 +0100

    Code Format

--- src/Knot3/KnotData/ChallengeMetaData.cs
+++ src/Knot3/KnotData/ChallengeMetaData.cs
@@ -161,9 +161,9 @@ namespace Knot3.KnotData
 			TimeSpan t = TimeSpan.FromSeconds ( secs );
 
 			string answer = string.Format ("{0:D2}h:{1:D2}m:{2:D2}s",
-			                              t.Hours,
-			                              t.Minutes,
-			                              t.Seconds);
+			                               t.Hours,
+			                               t.Minutes,
+			                               t.Seconds);
 			return answer;
 		}
 
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.143.0")]
-[assembly: AssemblyFileVersion("2.0.143.0")]
+[assembly: AssemblyVersion("2.0.144.0")]
+[assembly: AssemblyFileVersion("2.0.144.0")]
