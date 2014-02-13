13bd530c1d141e572f70aeb2a979bc39e133e7e1
Daniel Warzel <daniel.warzel@gmx.de>
2014-02-13 16:59:08 +0100

    KnotGenerator

---
+++ tests/MockObjects/KnotGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Knot3.KnotData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knot3.UnitTests.MockObjects
+{
+    class KnotGenerator
+    {
+        public static Knot generateValidSquaredKnot(int EdgeLength) 
+        {
+            Edge[] edgeList = new Edge[EdgeLength * 4];
+
+            for(int i = 0; i < EdgeLength; i++) {
+                edgeList[i] = Edge.Up;
+            }
+            for(int i = EdgeLength; i < EdgeLength*2; i++) {
+                edgeList[i] = Edge.Right;
+            }
+            for(int i = EdgeLength *2; i < EdgeLength*3; i++) {
+                edgeList[i] = Edge.Down;
+            }
+            for(int i = EdgeLength *3; i < EdgeLength*4; i++) {
+                edgeList[i] = Edge.Left;
+            }
+            KnotMetaData metaData = new KnotMetaData("SquareKnot", edgeList.Count<Edge>, null, null);
+            Knot squareKnot = new Knot(metaData, edgeList);
+            
+            return squareKnot;
+
+        }
+    }
+}
