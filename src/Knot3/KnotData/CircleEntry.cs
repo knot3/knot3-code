578b217da2ef51647c0481b6b206ef10334333af
Gerd Augsburg <g.augsburg@gmx.net>
2014-02-13 17:45:44 +0100

    Start to center

--- src/Knot3/KnotData/CircleEntry.cs
+++ src/Knot3/KnotData/CircleEntry.cs
@@ -184,6 +184,20 @@ namespace Knot3.KnotData
 			while (current != other.Next && current != this);
 		}
 
+        public IEnumerable<T> WayTo(T other)
+        {
+            CircleEntry<T> current = this;
+            while (!current.Value.Equals(other))
+            {
+                yield return current.Value;
+                current = current.Next;
+                if (current == this)
+                {
+                   break;
+                }
+            }
+        }
+
 		public IEnumerable<Tuple<T,T>> Pairs
 		{
 			get {
