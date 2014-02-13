f81243a6168a0c8eaabfed0e1feb26d8f3f41ed6
Daniel Warzel <daniel.warzel@gmx.de>
2014-02-13 17:05:52 +0100

    KnotStringIO

--- tests/Tests/KnotData/KnotStringIO_Tests.cs
+++ tests/Tests/KnotData/KnotStringIO_Tests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 
 using Knot3.KnotData;
+using Knot3.UnitTests.MockObjects;
 
 namespace Knot3.UnitTests.Tests.KnotData
 {
@@ -16,6 +17,8 @@ namespace Knot3.UnitTests.Tests.KnotData
 		[Test]
 		public void test_EncodeDecodeEdge ()
 		{
+            KnotStringIO knotStringIO = new KnotStringIO(KnotGenerator.generateValidSquaredKnot(10));
+            
 			Edge[] edges = { Edge.Left, Edge.Right, Edge.Up, Edge.Down, Edge.Forward, Edge.Backward };
 
 			foreach (Edge edge in edges) {
