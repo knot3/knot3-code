42f3261f3d0152cb4f7384f5173e79e8f2b0aaba
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-14 11:21:45 +0100

    Filter laufen immer noch nicht ...

--- coverage/OpenCover.bat
+++ coverage/OpenCover.bat
@@ -1,97 +1,130 @@
-::
-:: Stapelverarbeitungs-Skript für den Testabdeckungsbericht unter Windows (V. 1.1)
-::
-
 @echo off
+
+::
+:: Stapelverarbeitungs-Skript für den Testabdeckungsbericht unter Windows (V. 1.2)
+::
 echo.
 echo --- OpenCover.bat ---
 echo.
 
 ::
-:: Folgende Tools müssen installiert sein:
+:: Anleitung:
 ::
-:: !!!!!!!!!!!!!!!!!!!!!!!!!
+
 ::
-:: NUnit (2.6.3)
-:: OpenCover (4.5.1923)
-:: ReportGenerator (dort hin tun: %ProgramFiles(x86)%\ReportGenerator)
+:: -----------------------------------------------------------------------------
 ::
-:: !!!!!!!!!!!!!!!!!!!!!!!!!
+:: (1) Installieren von NUnit, OpenCover, ReportGenerator.
+:: (2) Visual Studio konfigurieren:
 ::
+:: In der Projektmappe mit der rechten Maustaste auf das Projekt klicken,
+:: welches später die Testabdeckung umfassen soll. Es erscheint ein Menü, 
+:: dort mit der linken Maustaste auf "Eigenschaften" klicken.
 ::
-:: Visual Studio: 
+:: Unter "Befehlszeile für Postbuildereignis" folgendes eingeben:
+:: call "$(SolutionDir)<Projektordner>\OpenCover.bat"
 ::
-:: 1. Einstellungen treffen: (Projektmappe)
+:: Unter "Postbuildereignis ausführen" den Eintrag "Immer" auswählen und 
+:: mit der linken Maustaste anklicken.
 ::
-:: NUnit-Test-Coverage-Windows -(Rechtsklick)-> Eigenschaften ->
+:: (3) Diese Stapelverarbeitungsdatei in das Projektverzeichnis legen.
+:: (4) Beim "Erstellen" des Projekts oder durch direktes Starten von 
+::     OpenCover.bat über einen Doppelklick, wird nun diese Stapel-
+::     verarbeitungsdatei ausgeführt und so der Testabdeckungsbericht 
+::     erstellt.
 ::
-:: Befehlszeile für Postbuildereignis : "$(SolutionDir)NUnit-Test-Coverage-Windows\OpenCover.bat"
-:: Postbuildereignis ausführen        : Immer
+:: -----------------------------------------------------------------------------
 ::
-:: 2. Erstellen: (Visual Studio Fenster, Menüzeile oben)
-::
-:: Erstellen -> NUnit-Test-Coverage-Windows neu erstellen
-::
-:: ...
-::
+
 echo.
 echo Settings:
 echo.
-echo Current path: %CD%
+echo Erstes Argument: "%1"
+echo.
 echo.
 
 ::
-:: Standard-Installationsverzeichnisse für die Werkzeuge verwenden!
+:: Projekt-Pfade, relativ zum Speicherort dieser Stapelverarbeitungsdatei.
 ::
 set PATH_TO_PROJECT=%~dp0..
-
-
-set PATH_TO_OPENCOVER=%LOCALAPPDATA%\Apps\OpenCover
-set PATH_TO_NUNIT=%ProgramFiles(x86)%\NUnit 2.6.3\bin
 set PATH_TO_TESTS=%PATH_TO_PROJECT%\tests\bin\Debug
-set PATH_TO_REPORTGENERATOR=%ProgramFiles(x86)%\ReportGenerator\bin
 set PATH_TO_REPORT=%PATH_TO_PROJECT%\coverage\bin\Debug
 
+::
+:: Standard-Installationsverzeichnisse der Werkzeuge:
+::
+set PATH_TO_NUNIT=%ProgramFiles(x86)%\NUnit 2.6.3\bin
+set PATH_TO_OPENCOVER=%LOCALAPPDATA%\Apps\OpenCover
+set PATH_TO_REPORTGENERATOR=%ProgramFiles(x86)%\ReportGenerator\bin
 
 
+::
+:: Konfigurationsdatei.
+:: Es ist möglich lokal (%USERPROFILE\Documents) eine Konfigurationsdatei anzugeben,
+:: in der die Pfade zu den Werkzeugen stehen. Alternativ können lokal auch harte Links
+:: verwendet werden ;)
+::
 set PATH_TO_CONFIGURATION_FILE=%USERPROFILE%\Documents\vs-crpc.txt
 
 if exist %PATH_TO_CONFIGURATION_FILE% (
 echo.
 echo Visual Studio coverage report path configuration file found.
 echo.
+
+for /F "eol=# tokens=1,2* delims==" %%i in (%PATH_TO_CONFIGURATION_FILE%) do (
+echo %%i %%j
+)
+
 ) else (
 echo.
 echo No configuration, using default settings.
 echo.
 )
 
-
-::
-:: Filter für OpenCover
-::
-set FILTER="+[*]* -[Knot3.Program]*"
-
-
-echo Paths to ...
 echo.
-echo  NUnit           : %PATH_TO_NUNIT%
-echo  Tests           : %PATH_TO_TESTS%
-echo  OpenCover       : %PATH_TO_OPENCOVER%
-echo  VS-Project      : %PATH_TO_PROJECT%
-echo  ReportGenerator : %PATH_TO_REPORTGENERATOR%
-echo  Report          : %PATH_TO_REPORT%
+echo Paths:
+echo.
+echo   Current         : %CD%
+echo   NUnit           : %PATH_TO_NUNIT%
+echo   Tests           : %PATH_TO_TESTS%
+echo   OpenCover       : %PATH_TO_OPENCOVER%
+echo   VS-Project      : %PATH_TO_PROJECT%
+echo   ReportGenerator : %PATH_TO_REPORTGENERATOR%
+echo   Report          : %PATH_TO_REPORT%
 echo.
 
+::
+:: Filter für OpenCover hier einstellen:
+::
+set FILTER="-filter:+Knot3.Widgets*"
+:: set FILTER="-filter:+[Knot3]* -Knot3.Widgets*"
+echo.
+echo OpenCover-Filter-Argument:
+echo.
+echo %FILTER%
+echo.
+
+echo ... Running tests, checking coverage ...
+echo.
 ::
 :: Hinweis: Auf einem 64-Bit-System ist nunit-console-x86.exe für 32-Bit-Projekte zu verwenden! (sonst tritt ein Fehler auf)
 ::
-"%PATH_TO_OPENCOVER%\OpenCover.Console.exe" -target:"%PATH_TO_NUNIT%\nunit-console-x86.exe" -targetargs:"/noshadow "%PATH_TO_TESTS%\Knot3.UnitTests.dll"" -register:user -filter:%FILTER% -output:"%PATH_TO_REPORT%\NUnit_test_coverage.xml"
-echo.
+"%PATH_TO_OPENCOVER%\OpenCover.Console.exe" -target:"%PATH_TO_NUNIT%\nunit-console-x86.exe" -targetargs:"/noshadow "%PATH_TO_TESTS%\Knot3.UnitTests.dll"" -register:user %FILTER% -output:"%PATH_TO_REPORT%\NUnit_test_coverage.xml">NUL
 
+echo ... Generating report ...
+echo.
 ::
-:: Ausgabe von ReportGenerator auf Konsole unterdrückt.
+:: Ausgabe von ReportGenerator auf der Konsole wird durch ">NUL" unterdrückt.
 ::
-"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" "%PATH_TO_REPORT%\NUnit_test_coverage.xml" "%PATH_TO_REPORT%"
-REM >NUL
+"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" "%PATH_TO_REPORT%\NUnit_test_coverage.xml" "%PATH_TO_REPORT%">NUL
+
+echo ... Showing report.
+echo.
+::
+:: Startet das Standardprogramm für das Betrachten von .htm:
+::
 start "" "%PATH_TO_REPORT%\index.htm"
+
+
+
+exit
