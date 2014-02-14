7dec0837697090f6948580cbc2b7d5e3664e0564
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-14 18:14:08 +0100

    Code Format

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion ("1.0.*")]
 
-[assembly: AssemblyVersion ("2.0.176.0")]
-[assembly: AssemblyFileVersion ("2.0.176.0")]
+[assembly: AssemblyVersion ("2.0.178.0")]
+[assembly: AssemblyFileVersion ("2.0.178.0")]
