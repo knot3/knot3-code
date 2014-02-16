e2eaae3beb2b92533ce8a0878bb6a5737630b79a
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-16 21:31:06 +0100

    Code Format

--- src/Knot3/Platform/SystemInfo-MonoGame.cs
+++ src/Knot3/Platform/SystemInfo-MonoGame.cs
@@ -69,7 +69,7 @@ namespace Knot3.Platform
 		public static bool IsRunningOnLinux ()
 		{
 			return Environment.OSVersion.Platform == PlatformID.Unix
-				|| Environment.OSVersion.Platform.ToString() == PlatformID.Unix.ToString();
+			       || Environment.OSVersion.Platform.ToString () == PlatformID.Unix.ToString ();
 		}
 
 		public static bool IsRunningOnWindows ()
