4df972443d257ef3e2e967bd5f3859a1f612b634
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-17 11:53:57 +0100

    OpenCover.bat veranlasst jetzt zusätzlich, dass LaTeX für den QS-Bericht generiert wird.

--- coverage/OpenCover.bat
+++ coverage/OpenCover.bat
@@ -48,7 +48,9 @@ echo.
 ::
 set PATH_TO_PROJECT=%~dp0..
 set PATH_TO_TESTS=%PATH_TO_PROJECT%\tests\bin\Debug
-set PATH_TO_REPORT=%PATH_TO_PROJECT%\coverage\bin\Debug
+set PATH_TO_RAW_REPORTDATA=%PATH_TO_PROJECT%\coverage\bin\Debug
+set PATH_TO_HTML_REPORT=%PATH_TO_PROJECT%\coverage\bin\Debug
+set PATH_TO_LATEX_REPORT=%PATH_TO_PROJECT%\..\knot-qualitaetssicherung\Bericht\Inhalt\Tests\Abdeckung
 
 ::
 :: Standard-Installationsverzeichnisse der Werkzeuge:
@@ -90,14 +92,15 @@ echo   Tests           : %PATH_TO_TESTS%
 echo   OpenCover       : %PATH_TO_OPENCOVER%
 echo   VS-Project      : %PATH_TO_PROJECT%
 echo   ReportGenerator : %PATH_TO_REPORTGENERATOR%
-echo   Report          : %PATH_TO_REPORT%
+echo   Raw-Reportdata  : %PATH_TO_RAW_REPORTDATA%
+echo   HTML-Report     : %PATH_TO_HTML_REPORT%
+echo   LaTeX-Report    : %PATH_TO_LATEX_REPORT%
 echo.
 
 ::
 :: Filter für OpenCover hier einstellen:
 ::
 set FILTER=+[Knot3]* -[Knot3]Knot3.Program -[Knot3]Knot3.Development.* -[Knot3]Knot3.Widgets.* -[Knot3]Knot3.Screens.* -[Knot3]Knot3.RenderEffects.* -[Knot3]Knot3.GameObjects.* -[Knot3]Knot3.Input.* -[Knot3]Knot3.Utilities.ShaderHelper -[Knot3]Knot3.Utilities.ModelHelper -[Knot3]Knot3.Core.Knot3Game -[Knot3]Knot3.Core.GameScreen
-:: set FILTER="-filter:+[Knot3]* -Knot3.Widgets*"
 echo.
 echo OpenCover-Filter-Argument:
 echo.
@@ -109,22 +112,32 @@ echo.
 ::
 :: Hinweis: Auf einem 64-Bit-System ist nunit-console-x86.exe für 32-Bit-Projekte zu verwenden! (sonst tritt ein Fehler auf)
 ::
-"%PATH_TO_OPENCOVER%\OpenCover.Console.exe" -target:"%PATH_TO_NUNIT%\nunit-console-x86.exe" -targetargs:"/noshadow "%PATH_TO_TESTS%\Knot3.UnitTests.dll"" -register:user -filter:"%FILTER%" -output:"%PATH_TO_REPORT%\NUnit_test_coverage.xml">NUL
+"%PATH_TO_OPENCOVER%\OpenCover.Console.exe" -target:"%PATH_TO_NUNIT%\nunit-console-x86.exe" -targetargs:"/noshadow "%PATH_TO_TESTS%\Knot3.UnitTests.dll"" -register:user -filter:"%FILTER%" -output:"%PATH_TO_RAW_REPORTDATA%\NUnit_test_coverage.xml">NUL
 
 echo ... Generating report ...
 echo.
+echo - HTML
+echo.
 ::
 :: Ausgabe von ReportGenerator auf der Konsole wird durch ">NUL" unterdrückt.
+:: Erstellung des Berichts als Html:
 ::
-"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" "%PATH_TO_REPORT%\NUnit_test_coverage.xml" "%PATH_TO_REPORT%">NUL
+"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" -reporttypes:"Html" -reports:"%PATH_TO_RAW_REPORTDATA%\NUnit_test_coverage.xml" -targetdir:"%PATH_TO_HTML_REPORT%"
+
+echo - LaTeX
+echo.
+::
+:: Erstellung des Berichts als LaTeX:
+::
+"%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe" -reporttypes:"Latex" -reports:"%PATH_TO_RAW_REPORTDATA%\NUnit_test_coverage.xml" -targetdir:"%PATH_TO_LATEX_REPORT%"
+
 
 echo ... Showing report.
 echo.
 ::
 :: Startet das Standardprogramm für das Betrachten von .htm:
 ::
-start "" "%PATH_TO_REPORT%\index.htm"
+start "" "%PATH_TO_HTML_REPORT%\index.htm"
 
-
-
+pause
 exit
