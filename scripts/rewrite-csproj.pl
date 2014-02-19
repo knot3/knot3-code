f4a91c3a74dafefc3c6c1b82978d73227d19fd75
Tobias Schulz <tobias@freehal.net>
2014-02-19 22:45:47 +0100

    ...

--- scripts/rewrite-csproj.pl
+++ scripts/rewrite-csproj.pl
@@ -50,6 +50,8 @@ sub rewrite_csproj {
 my @csproj_files = (
 	{ csproj => 'src/Knot3.Game-MonoGame.csproj', dir => 'src/', exclude => ['-XNA.cs'], linesep => qq[\n] },
 	{ csproj => 'src/Knot3.Game-XNA.csproj', dir => 'src/', exclude => ['-MonoGame.cs'], linesep => qq[\n] },
+	{ csproj => 'framework/Knot3.Framework-MonoGame.csproj', dir => 'src/', exclude => ['-XNA.cs'], linesep => qq[\n] },
+	{ csproj => 'framework/Knot3.Framework-XNA.csproj', dir => 'src/', exclude => ['-MonoGame.cs'], linesep => qq[\n] },
 	{ csproj => 'tests/Knot3.UnitTests-MonoGame.csproj', dir => 'tests/', exclude => [], linesep => qq[\n] },
 	{ csproj => 'tests/Knot3.UnitTests-XNA.csproj', dir => 'tests/', exclude => [], linesep => qq[\n] },
 	{ csproj => 'tools/ConfigReset/Knot3.ConfigReset.csproj', dir => 'tools/ConfigReset/', exclude => [], linesep => qq[\n] },
