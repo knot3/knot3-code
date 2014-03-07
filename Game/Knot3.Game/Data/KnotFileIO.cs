2bb6120d205ddd7e4f4164753989192f53d79ca5
Tobias Schulz <tobias@freehal.net>
2014-03-07 17:17:08 +0100

    Code Format

--- Game/Knot3.Game/Data/KnotFileIO.cs
+++ Game/Knot3.Game/Data/KnotFileIO.cs
@@ -27,9 +27,11 @@
  * 
  * See the LICENSE file for full license details of the Knot3 project.
  */
+
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+
 using Knot3.Framework.Platform;
 using Knot3.Framework.Storage;
 
