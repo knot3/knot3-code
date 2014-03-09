1df54be01e1e66c614edd7c4de1f46988dac44ec
Tobias Schulz <tobias@freehal.net>
2014-03-09 22:10:11 +0100

    Code Format

--- Game/Properties/AssemblyInfo.cs
+++ Game/Properties/AssemblyInfo.cs
@@ -66,5 +66,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion ("1.0.*")]
 
-[assembly: AssemblyVersion ("1.4.66.0")]
-[assembly: AssemblyFileVersion ("1.4.66.0")]
+[assembly: AssemblyVersion ("1.4.68.0")]
+[assembly: AssemblyFileVersion ("1.4.68.0")]
